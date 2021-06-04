8f980d808ec9f3aa1cc92f9a8f80684ee88b478f
Lutz Rothenbusch <lutz@rothenserv.de>
2021-06-04 13:07:18 +0200

    Drawpool works

--- src/dummyconsumer/main.cxx
+++ src/dummyconsumer/main.cxx
@@ -1,9 +1,34 @@
 #include <iostream>
+#include <vector>
 #include "Lazyguy.h"
-#include "Drawpool.h"
+// #include "Drawpool.h"
 
-using Dummylib::Drawpool;
-template class Dummylib::Drawpool<int>;
+// template <class T>
+// class myPair{
+//     T values [2];
+// public:
+//     myPair(T first, T second)
+//     {
+//         values[0] = first;
+//         values[1] = second;
+//     }
+// };
+
+template<class T>
+class Drawpool{
+private:
+    std::vector<T> mPool;
+public:
+    Drawpool(std::vector<T> iData){
+        this->mPool = iData;
+        srand (time(NULL));
+    }
+    T drawElement(){
+        return mPool[0];
+    }
+};
+
+
 int main(int argc, char* argv[]){
     Dummylib::Lazyguy l;
     if(!l.doNothing(2u)){
@@ -11,8 +36,11 @@ int main(int argc, char* argv[]){
     }
 
 
+//     myPair<int> a(2,3);
+
     std::vector<int> wMyVector{2, 3 , 1, 4};
-    Drawpool<int> myDrawpool(wMyVector);
+   Drawpool<int> myDrawpool(wMyVector);
     int bla = myDrawpool.drawElement();
+    std::cout<<"Drew "<<bla<<std::endl;
     return 0;
 }
